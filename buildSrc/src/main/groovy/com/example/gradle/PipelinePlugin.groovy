bf208ffbe9d212b352e1a312d96d4dbd3b96a540
Jens Fischer <jens.fischer@otto.de>
2021-05-11 22:35:27 +0200

    extract Gradle plugin

---
+++ buildSrc/src/main/groovy/com/example/gradle/PipelinePlugin.groovy
@@ -0,0 +1,47 @@
+package com.example.gradle
+
+import groovy.transform.CompileStatic
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+import java.security.SecureRandom
+import java.util.concurrent.TimeUnit
+
+@CompileStatic
+class PipelinePlugin implements Plugin<Project> {
+
+    SecureRandom random = new SecureRandom()
+
+    @Override
+    void apply(Project project) {
+
+        project.tasks.register('deployCI') {
+            it.group = "pipeline"
+            it.doLast {
+                sleep(1000, 5000)
+            }
+        }
+
+        project.tasks.register('deployStaging') {
+            it.group = "pipeline"
+            it.doLast {
+                sleep(1000, 5000)
+            }
+        }
+
+        project.tasks.register('deployProduction') {
+            it.group = "pipeline"
+            it.doLast {
+                sleep(1000, 5000)
+            }
+        }
+    }
+
+    int random(int min, int max) {
+        return min + random.nextInt((max - min) + 1);
+    }
+
+    void sleep(int min, int max) {
+        TimeUnit.MILLISECONDS.sleep(random(min, max))
+    }
+}
